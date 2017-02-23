007caaf43448ea61f5b50acdcee3b0f6f1c49f86
yourcoke <deinkoks@gmail.com>
2017-02-23 20:57:15 +0100

    moved patterns, changed arguments to optional

---
+++ settings.py
@@ -0,0 +1,14 @@
+PATTERNS = [
+    {'SUB': 'foobar/oben/lounge/leinwand/action',
+     'TAGS': {'TOPIC': 'TOPIC', 'ROOM': -3},
+     'MEASUREMENT': "leinwand", 'TYPE': str
+     },
+    {'SUB': 'foobar/+/tuer',
+     'TAGS': {'location': -2},
+     'MEASUREMENT': "door_status", 'TYPE': int
+     },
+    {'SUB': '/hq/og/baellebad/sensoren/temperatur',
+     'TAGS': {'location': -3},
+     'MEASUREMENT': "temperature", 'TYPE': float
+     }
+]
--- tremendousswampy.py
+++ tremendousswampy.py
@@ -4,20 +4,8 @@ from influxdb import InfluxDBClient
 
 from lib.swamp import Swamp
 
-DEFAULT_PATTERNS = [
-    {'SUB': 'foobar/oben/lounge/leinwand/action',
-     'TAGS': {'TOPIC': 'TOPIC', 'ROOM': -3},
-     'MEASUREMENT': "leinwand", 'TYPE': str
-     },
-    {'SUB': 'foobar/+/tuer',
-     'TAGS': {'location': -2},
-     'MEASUREMENT': "door_status", 'TYPE': int
-     },
-    {'SUB': '/hq/og/baellebad/sensoren/temperatur',
-     'TAGS': {'location': -3},
-     'MEASUREMENT': "temperature", 'TYPE': float
-     }
-]
+from settings import PATTERNS
+
 
 # all the cmdline foo
 parser = argparse.ArgumentParser(
@@ -28,20 +16,20 @@ parser.add_argument('--test', action="store_true",
 parser.add_argument('--run', action="store_true",
                     dest="run", help='runs the module as daemon')
 
-parser.add_argument("mqtthost", type=str, help="MQTT Host",
+parser.add_argument("--mqtthost", type=str, help="MQTT Host",
                     default="localhost")
-parser.add_argument("mqttport", type=int, help="MQTT Port", default=1883)
+parser.add_argument("--mqttport", type=int, help="MQTT Port", default=1883)
 
 
-parser.add_argument("influxhost", type=str,
+parser.add_argument("--influxhost", type=str,
                     help="influxdb Host", default="localhost")
-parser.add_argument("influxport", type=int, help="influxdb Port", default=8086)
+parser.add_argument("--influxport", type=int, help="influxdb Port", default=8086)
 
-parser.add_argument("influxuser", type=str,
+parser.add_argument("--influxuser", type=str,
                     help="influxdb User")
-parser.add_argument("influxpass", type=str,
+parser.add_argument("--influxpass", type=str,
                     help="influxdb Pass")
-parser.add_argument("influxdb", type=str,
+parser.add_argument("--influxdb", type=str,
                     help="influxdb Database")
 args = parser.parse_args()
 if args.run:
@@ -57,7 +45,7 @@ if args.run:
     influx_client = InfluxDBClient(
         args.influxhost, args.influxport, args.influxuser, args.influxpass, args.influxdb)
     # Swamp stuff
-    swamp = Swamp(mqtt_client, influx_client, DEFAULT_PATTERNS)
+    swamp = Swamp(mqtt_client, influx_client, PATTERNS)
     swamp.subscribe()
     swamp.loop()
 elif args.test:
